de0b516e4be54f4c72c609ccdd6bcd04315ad1bc
Nitesh822 <38714672+Nitesh822@users.noreply.github.com>
2019-06-11 12:54:19 +0530

    Test code pushed to repository.

---
+++ Area of a square/Main.java
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+  int side=9,Area;
+  Area=side*side;
+  printf("%d sq cm",Area);
+  return 0;
+}
---
+++ Multiply two numbers/Main.java
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+  int a=63,b=81, mul=0;
+  mul=a*b;
+  printf("%d",mul);
+  return 0;
+}
